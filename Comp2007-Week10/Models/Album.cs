0eedb5d34d02fc50aaf8908b4a725930fd248395
Emdaisy1 <hilborn_emmav@live.ca>
2016-07-27 15:54:17 -0400

    Data validations for validation and display enhancements Addressses issue #13

--- Comp2007-Week10/Models/Album.cs
+++ Comp2007-Week10/Models/Album.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Comp2007_Week10.Models
 {
+    [Bind(Exclude = "AlbumId")]
     public class Album
     {
 
@@ -24,12 +28,15 @@ namespace Comp2007_Week10.Models
         {
             this.Title = Title;
         }
-
+        [ScaffoldColumn(false)]
         public virtual int AlbumId { get; set; }
+        [DisplayName("Genre")]
         public virtual int GenreId { get; set; }
+        [DisplayName("Artist")]
         public virtual int ArtistId { get; set; }
         public virtual string Title { get; set; }
         public virtual decimal Price { get; set; }
+        [DisplayName("Album Art URL")]
         public virtual string AlbumArtUrl { get; set; }
         public virtual Genre Genre { get; set; }
         public virtual Artist Artist { get; set; }
