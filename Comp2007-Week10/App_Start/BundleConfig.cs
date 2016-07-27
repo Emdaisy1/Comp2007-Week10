9a843d080eb5006240f86332a6a7f49d49082651
Emdaisy1 <hilborn_emmav@live.ca>
2016-07-27 14:58:58 -0400

    Modify Scaffolded Views Addresses issue #11

--- Comp2007-Week10/App_Start/BundleConfig.cs
+++ Comp2007-Week10/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace Comp2007_Week10
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
