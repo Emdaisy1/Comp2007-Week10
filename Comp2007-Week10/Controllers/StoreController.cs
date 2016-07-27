2b4ce73997213b7f9ddfa1a4cba357be31206232
Emdaisy1 <hilborn_emmav@live.ca>
2016-07-26 21:32:45 -0400

    Update StoreController Browse Action method to use live data Update StoreController Details Action method to use live data Addresses issues #8 and #9

--- Comp2007-Week10/Controllers/StoreController.cs
+++ Comp2007-Week10/Controllers/StoreController.cs
@@ -19,16 +19,19 @@ namespace Comp2007_Week10.Controllers
         }        
         //
         // GET: /Store/Browse?genre=Disco
-        public ActionResult Browse(string genre)
+        public ActionResult Browse(string genre = "Alternative")
         {
-            Genre genreModel = new Genre(genre);
+            // Retrieve Genre (single instance) and its Associated Albums from database
+            Genre genreModel = storeDB.Genres.Include("Albums").Single(g => g.Name == genre);
+
             return View(genreModel);
         }
         //
         // GET: /Store/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(int id = 1)
         {
-            Album album = new Album("Album " + id);
+            Album album = storeDB.Albums.Find(id);
+
             return View(album);
         }
     }
